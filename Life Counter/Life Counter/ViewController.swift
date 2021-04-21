cc89955911ab2db6ed4b519ec5111090aab7de62
Jaimie Jin <jinjaimie12@gmail.com>
2021-04-21 13:41:55 -0700

    score changing working for all players

--- Life Counter/Life Counter/ViewController.swift
+++ Life Counter/Life Counter/ViewController.swift
@@ -8,18 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var play1: UILabel!
-    @IBOutlet var play2: UILabel!
-    @IBOutlet var play3: UILabel!
-    @IBOutlet var play4: UILabel!
     @IBOutlet var losingMessage: UILabel!
     
-    var scores = [20, 20, 20, 20];
+    var scores = [20, 20, 20, 20]
+    @IBOutlet var buttons: [UILabel]!
+    var player = ["Player 1", "Player 2", "Player 3", "Player 4"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         losingMessage.isHidden = true;
     }
+    
     func checkScore(score: Int, player: String) {
         if (score <= 0) {
             losingMessage.text = "\(player) LOSES!"
@@ -27,23 +26,23 @@ class ViewController: UIViewController {
         }
     }
     //Player 1
-    @IBAction func play1Add1(_ sender: Any) {
-        scores[0] += 1;
-        play1.text = String(scores[0])
+    @IBAction func Add1(_ sender: UIButton) {
+        scores[sender.tag] += 1;
+        buttons[sender.tag].text = String(scores[sender.tag])
     }
-    @IBAction func play1Add5(_ sender: Any) {
-        scores[0] += 5;
-        play1.text = String(scores[0])
+    @IBAction func Add5(_ sender: UIButton) {
+        scores[sender.tag] += 5;
+        buttons[sender.tag].text = String(scores[sender.tag])
     }
-    @IBAction func play1Sub1(_ sender: Any) {
-        scores[0] -= 1;
-        play1.text = String(scores[0])
-        checkScore(score: scores[0], player: "Player 1")
+    @IBAction func Sub1(_ sender: UIButton) {
+        scores[sender.tag] -= 1;
+        buttons[sender.tag].text = String(scores[sender.tag])
+        checkScore(score: scores[sender.tag], player: player[sender.tag])
     }
-    @IBAction func play1Sub5(_ sender: Any) {
-        scores[0] -= 5;
-        play1.text = String(scores[0])
-        checkScore(score: scores[0], player: "Player 1")
+    @IBAction func Sub5(_ sender: UIButton) {
+        scores[sender.tag] -= 5;
+        buttons[sender.tag].text = String(scores[sender.tag])
+        checkScore(score: scores[sender.tag], player: player[sender.tag])
     }
 }
 
