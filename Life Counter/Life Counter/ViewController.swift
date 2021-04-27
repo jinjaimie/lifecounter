5146f6c61a91695ad860dd79e2792b3fda76c31d
Jaimie Jin <jinjaimie12@gmail.com>
2021-04-26 17:10:26 -0700

    Add/Subtract Players

--- Life Counter/Life Counter/ViewController.swift
+++ Life Counter/Life Counter/ViewController.swift
@@ -9,16 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var losingMessage: UILabel!
-    
-    var scores = [20, 20, 20, 20]
+    var numPlayer = 4;
+    var scores = [20, 20, 20, 20, 20, 20, 20, 20]
     @IBOutlet var buttons: [UILabel]!
-    var player = ["Player 1", "Player 2", "Player 3", "Player 4"]
+    @IBOutlet var addPButton: UIButton!
+    @IBOutlet var SubPButton: UIButton!
+    @IBOutlet var playerList: [UIStackView]!
+    var players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         losingMessage.isHidden = true;
+//        for _ in 1...4 {
+//            totalView.addSubview(onePlayer)
+//        }
+        for i in 4...7 {
+            for p in playerList {
+                if p.tag == i {
+                    p.isHidden = true;
+                }
+            }
+        }
     }
-    
     func checkScore(score: Int, player: String) {
         if (score <= 0) {
             losingMessage.text = "\(player) LOSES!"
@@ -36,30 +48,71 @@ class ViewController: UIViewController {
     }
 
     @IBAction func Add1(_ sender: UIButton) {
+        addPButton.isEnabled = false;
+        SubPButton.isEnabled = false;
         losingMessage.isHidden = true;
         scores[sender.tag] += 1;
         let button = findScore(sender.tag);
         button.text = String(scores[sender.tag])
     }
     @IBAction func Add5(_ sender: UIButton) {
+        addPButton.isEnabled = false;
+        SubPButton.isEnabled = false;
         losingMessage.isHidden = true;
         scores[sender.tag] += 5;
         let button = findScore(sender.tag);
         button.text = String(scores[sender.tag])
     }
     @IBAction func Sub1(_ sender: UIButton) {
+        addPButton.isEnabled = false;
+        SubPButton.isEnabled = false;
         losingMessage.isHidden = true;
         scores[sender.tag] -= 1;
         let button = findScore(sender.tag);
         button.text = String(scores[sender.tag])
-        checkScore(score: scores[sender.tag], player: player[sender.tag])
+        checkScore(score: scores[sender.tag], player: players[sender.tag])
     }
     @IBAction func Sub5(_ sender: UIButton) {
+        addPButton.isEnabled = false;
+        SubPButton.isEnabled = false;
         losingMessage.isHidden = true;
         scores[sender.tag] -= 5;
         let button = findScore(sender.tag);
         button.text = String(scores[sender.tag])
-        checkScore(score: scores[sender.tag], player: player[sender.tag])
+        checkScore(score: scores[sender.tag], player: players[sender.tag])
     }
+
+    @IBAction func addPlayer(_ sender: UIButton) {
+        for p in playerList {
+            if p.tag == numPlayer {
+                p.isHidden = false;
+                numPlayer += 1;
+                break;
+            }
+        }
+        if numPlayer == 8 {
+            addPButton.isEnabled = false;
+        }
+        if numPlayer != 2 {
+            SubPButton.isEnabled = true;
+        }
+    }
+    
+    @IBAction func MinusPlayer(_ sender: UIButton) {
+        for p in playerList {
+            if p.tag == numPlayer - 1 {
+                p.isHidden = true;
+                numPlayer -= 1;
+                break;
+            }
+        }
+        if numPlayer != 8 {
+            addPButton.isEnabled = true;
+        }
+        if numPlayer == 2 {
+            SubPButton.isEnabled = false;
+        }
+    }
+
 }
 
---
+++ Life Counter/Life Counter/ViewControllerHistory.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerHistory.swift
+//  Life Counter
+//
+//  Created by Jaimie Jin on 4/26/21.
+//
+
+import UIKit
+
+class ViewControllerHistory: UIViewController {
+    
+}
+
